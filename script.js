df9548fff54f62322278354287855ac8391002c8
andriimarmarosovych <andrewmartsyniuk@gmail.com>
2023-05-09 19:15:47 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function changeColorBlue() {
+	document.body.style.backgroundColor = "blue";
+}
+
+function changeColorPink() {
+	document.body.style.backgroundColor = "pink";
+}
+
+function changeColorBrown() {
+	document.body.style.backgroundColor = "brown";
+}
+
+function changeColorWhite() {
+	document.body.style.backgroundColor = "white";
+}
+
+function changeColorYellow() {
+	document.body.style.backgroundColor = "yellow";
+}
